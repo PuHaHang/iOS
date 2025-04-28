d53e10aca4d77860dd50d38233a0164655d4f78d
LURKS02 <thecrooks2@gmail.com>
2025-04-28 12:21:25 +0900

    [CHORE]: SwiftLint 룰 추가 및 적용

--- Project.swift
+++ Project.swift
@@ -18,6 +18,24 @@ let project = Project(
             ),
             sources: ["PuHaHang/Sources/**"],
             resources: ["PuHaHang/Resources/**"],
+            scripts: [
+                .pre(script: """
+        if [ -d "$HOME/.local/share/mise/shims" ]; then
+            export PATH="$HOME/.local/share/mise/shims:$PATH"
+        else
+            echo "mise shims directory not found"
+        fi
+        
+        SWIFTLINT_PATH=$(which swiftlint)
+        SWIFTLINT_CONFIG_PATH="$SRCROOT/.swiftlint.yml"
+        
+        if [ -n "$SWIFTLINT_PATH" ]; then
+            swiftlint --config "$SWIFTLINT_CONFIG_PATH"
+        else
+            echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
+        fi
+        """, name: "SwiftLint")
+            ],
             dependencies: []
         ),
         .target(
