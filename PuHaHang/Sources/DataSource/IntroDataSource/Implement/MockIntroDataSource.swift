d78c520aab5a2f3a406d0bdecfcef2d94421adc4
LURKS02 <thecrooks2@gmail.com>
2025-05-19 16:03:12 +0900

    [FEAT]: IntroDataSource 구현

--- PuHaHang/Sources/DataSource/IntroDataSource/Implement/MockIntroDataSource.swift
+++ PuHaHang/Sources/DataSource/IntroDataSource/Implement/MockIntroDataSource.swift
@@ -6,7 +6,17 @@
 //
 
 import Foundation
+import Minifire
 
 final class MockIntroDataSource: IntroDataSource {
-    
+    func fetchVersionRequirement() async throws -> VersionRequirementDTO {
+        let request = MFRequest(url: "http://121.157.24.40/v1/intro")
+        let response = try await MF.requestDecodable(of: VersionRequirementDTO.self, request)
+        
+        guard response.httpResponse?.statusCode == 200 else {
+            throw LocalError(message: "Error: Invalid status code")
+        }
+        
+        return response.value
+    }
 }
