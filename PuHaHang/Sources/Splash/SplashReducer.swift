91e661a6d77014f556ecebc9e1e1aa64389c4366
LURKS02 <thecrooks2@gmail.com>
2025-05-28 02:57:54 +0900

    [REFACTOR]: Action 내 동작을 함수로 분리

--- PuHaHang/Sources/Splash/SplashReducer.swift
+++ PuHaHang/Sources/Splash/SplashReducer.swift
@@ -26,26 +26,39 @@ struct SplashReducer {
         Reduce { state, action in
             switch action {
             case .onAppear:
-                return .run { send in
-                    do {
-                        let intro: Intro = try await introRepository.fetchIntro()
-                        await send(.introResponse(.success(intro)))
-                    } catch {
-                        await send(.introResponse(.failure(error)))
-                    }
-                }
+                return reduceOnAppear()
                 
             case .introResponse(let result):
-                switch result {
-                case .success(let intro):
-                    state.introRoadState = .success(intro)
-                    
-                case .failure(let error):
-                    state.introRoadState = .failure(error)
-                }
-                
-                return .none
+                return reduceIntroResponse(&state, result: result)
             }
         }
+    }
+}
+
+private extension SplashReducer {
+    func reduceOnAppear() -> Effect<Action> {
+        .run { send in
+            do {
+                let intro: Intro = try await introRepository.fetchIntro()
+                await send(.introResponse(.success(intro)))
+            } catch {
+                await send(.introResponse(.failure(error)))
+            }
+        }
+    }
+    
+    func reduceIntroResponse(
+        _ state: inout SplashReducer.State,
+        result: Result<Intro, Error>
+    ) -> Effect<Action> {
+        switch result {
+        case .success(let intro):
+            state.introRoadState = .success(intro)
+            
+        case .failure(let error):
+            state.introRoadState = .failure(error)
+        }
+        
+        return .none
     }
 }
